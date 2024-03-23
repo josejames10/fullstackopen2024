59ea272516ab8d47a12757997e6b62d5b83e7054
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-03-22 20:32:09 -0500

    estado del componente y controladores de eventos

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
 
+const Display = ({counte}) => <div>{counte}</div>  
+
+
+const Button = (prosp) => {
+  return (<button onClick={prosp.onClick}>{prosp.text}</button>)
+}
+
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
 
+  const increaseByOne = () => setCounter(counter+1)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const decreaseByOne = () => setCounter(counter-1)
+
+  const setToZero = () => setCounter(0)
+  
 
   return (
-    <div>{counter}</div>
+    <div>
+    <Display counte={counter} />
+    <Button onClick={increaseByOne} text="plus"/>
+    <Button onClick={decreaseByOne} text="minus"/>
+    <Button onClick={setToZero} text="zero"/>
+
+    </div>
   )
 }
 
