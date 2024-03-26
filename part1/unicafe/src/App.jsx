35a97acce760fd0f75b874c0dc4ace9db60a75dc
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-03-26 15:39:30 -0500

    1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,55 @@
 import { useState } from 'react'
-
-const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Date =(props) =>{
+    return(
+      <div>
+        {props.text}{props.value}
+      </div>
+    )
+  }
+/* const Button = (props) =>{
   return (
     <div>
-      code here
+      <button onClick={props.onClick}>{props.text}</button>  
+    </div>
+  )
+  } */
+  const App = () => {
+    // guarda los clics de cada botón en su propio estado
+{/* <Button onClick={addGood} text="good"></Button>
+<Button onClick={addNeutral} text="Neutral"></Button>
+<Button onClick={addBad} text="bad"></Button> */}
+  
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const addGood = () =>{
+      setGood(good+1)      
+    }
+    const addNeutral = () =>{
+      setNeutral(neutral+1) 
+    }
+    const addBad = () =>{
+      setBad(bad+1) 
+    }
+    return (  
+    <div>
+      <h1>give feeback</h1>
+      <button onClick={addGood}>
+        goog
+      </button>
+      <button onClick={addNeutral}>
+        neutral
+      </button>
+      <button onClick={addBad}>
+        bad
+      </button>
+
+      <h1>statistics</h1>
+      <Date text="good " value={good}></Date>
+      <Date text="neutral " value={neutral}></Date>
+      <Date text="bad " value={bad}></Date>
+
+    
     </div>
   )
 }
