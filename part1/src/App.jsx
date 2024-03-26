532147910a8382988a855df9876ccf0198c9de2a
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-03-25 19:46:30 -0500

    inicio de unicafe

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,21 +9,33 @@ const Button = (prosp) => {
 
 const App = () => {
 
-  const [ counter, setCounter ] = useState(0)
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight] = useState(0)
+  const [ allClicks, setAll] = useState([]) 
+  const [ total, setTotal] = useState(0)
 
-  const increaseByOne = () => setCounter(counter+1)
+  const handleLeftClick = () =>{
+      setAll(allClicks.concat('L'))
+      setLeft(left + 1)
+      console.log(left)
+      setTotal(left + right)
+      console.log(left)
+  }
 
-  const decreaseByOne = () => setCounter(counter-1)
+  const handleRightClick = () => {
+      setAll(allClicks.concat('R'))
+      setRight(right + 1)
+      setTotal(right + left)
+  }
+    return (
 
-  const setToZero = () => setCounter(0)
-  
-
-  return (
     <div>
-    <Display counte={counter} />
-    <Button onClick={increaseByOne} text="plus"/>
-    <Button onClick={decreaseByOne} text="minus"/>
-    <Button onClick={setToZero} text="zero"/>
+    {left}
+    <Button onClick={handleLeftClick} text="Left"/>
+    <Button onClick={handleRightClick} text="Right"/>
+    {right}
+    <p>{allClicks.join(' ')}</p>
+    <p>total {total}</p>
 
     </div>
   )
