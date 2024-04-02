aa63bae720ad5ac43eb069c6edcac4554aae5982
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-04-01 21:01:05 -0500

    added exercise 2.2

--- part2/courseinfo/src/components/Courses.jsx
+++ part2/courseinfo/src/components/Courses.jsx
@@ -1,15 +1,12 @@
-
-
 const Course = ({course}) => {
-    console.log(course.name)
+    console.log(course.parts)
     
     return (
         <div>
-
             <Header header={course.name}></Header>
             <Content parts={course.parts}></Content>
+            <Total parts={course.parts}></Total>
         </div>
-
         )
 }
 
@@ -20,8 +17,11 @@ const Content = ({parts}) =>
         <Part part={parts[0]}></Part>
         <Part part={parts[1]}></Part>
         <Part part={parts[2]}></Part>
+        <Part part={parts[3]}></Part>
     </>
     
 const Part = ({part}) =><div> {part.name}: {part.exercises}</div>
+
+const Total = ({parts}) =><b>total of {parts[0].exercises+parts[1].exercises+parts[2].exercises+parts[3].exercises } exercises</b>
 
 export default Course;
