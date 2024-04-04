da42249ec8bb9fd561a88cb3303908305d87ef69
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-04-03 21:38:52 -0500

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
-import Numbers from "./components/Numbers";
+import Numbers from "./components/Contact";
 
-const App = (props) => {
+const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: "0", number: "040 - 1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 0 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 1 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 2 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 3 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const handleNewName = (e) => {
-    setNewName(e.target.value);
-  };
-  const handleNewNumber = (e) => {
-    setNewNumber(e.target.value);
-  };
+  const [newFilter, setNewFilter] = useState("");
+
+  const handleNewName = (e) => setNewName(e.target.value);
+  const handleNewNumber = (e) => setNewNumber(e.target.value);
+  const handleFilter = (e) => setNewFilter(e.target.value);
+
+  const filter = persons.map((e) =>
+    e.name.toLowerCase().includes(newFilter.toLowerCase())
+  )
+    ? persons.filter((e) => e.name.toLowerCase().includes(newFilter))
+    : persons;
+  console.log(filter);
 
   const add = (e) => {
     e.preventDefault();
-    //    console.log(newName);
     const newObjeto = { name: newName, id: persons.length, number: newNumber };
     let pre = true;
     persons.map((e) => {
-      //      console.log(e.name);
       if (e.name === newName) {
         console.log("nombre usado");
         pre = false;
@@ -31,11 +38,12 @@ const App = (props) => {
       ? setPersons(persons.concat(newObjeto))
       : alert(`${newName} is already added to phonebook`);
   };
-  console.log(persons.number);
+
   return (
     <div>
       <h2>Phonebook</h2>
-
+      filter : <input onChange={handleFilter} />
+      <h3>add a new </h3>
       <form onSubmit={add}>
         <div>
           name: <input onChange={handleNewName} />
@@ -48,7 +56,7 @@ const App = (props) => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((e) => (
+      {filter.map((e) => (
         <Numbers name={e.name} number={e.number} key={e.id}></Numbers>
       ))}
     </div>
