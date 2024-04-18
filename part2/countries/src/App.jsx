d893cc8bcbfcb43dd6d7402962689c4b2008699d
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-04-18 11:41:47 -0500

    exercises 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-const Content = ({ filter }) => {
+const Content = ({ filter,handleShow }) => {
   const fl = filter.length;
   if (fl >= 10) {
     return <div>Too many matches, specify another filter</div>;
@@ -11,13 +11,15 @@ const Content = ({ filter }) => {
   return (
     <>
       {filter.map((filter, index) => (
-        <Paises name={filter.name.common} key={index}></Paises>
+        <Paises name={filter.name.common} key={index} handleShow={handleShow} pais={filter}></Paises>
       ))}
     </>
   );
 };
-const Paises = ({ name }) => {
-  return <div>{name}</div>;
+const Paises = ({ name,handleShow,pais }) => {
+  return (
+  <div>{name}<button onClick={()=>handleShow(pais)}>show</button></div>
+)
 };
 const Pais = ({ pais }) => {
   const languages = Object.keys(pais.languages);
@@ -38,6 +40,7 @@ const Pais = ({ pais }) => {
 function App() {
   const [value, setValue] = useState("");
   const [countries, setCountries] = useState([]);
+  const [show , setshow] = useState(null);
 
   useEffect(() => {
     axios
@@ -50,7 +53,12 @@ function App() {
   const handleChange = (event) => {
     setValue(event.target.value);
   };
-
+  const handleShow = (p) => {
+    setshow(<Pais pais={p}></Pais>)
+    setTimeout(() => {
+      setshow(null)
+    }, 5000);
+  };
   const onSearch = (event) => {
     event.preventDefault();
   };
@@ -67,7 +75,8 @@ function App() {
       <form onSubmit={onSearch}>
         find countries: <input value={value} onChange={handleChange} />
       </form>
-      <Content filter={filter}></Content>
+      {show}
+      <Content filter={filter} handleShow={handleShow}></Content>
     </div>
   );
 }
