c00d55c6ef525dcf318c0d58424efeb154a766d7
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-04-23 17:14:02 -0500

    exercises 3.5

--- part3/index.js
+++ part3/index.js
@@ -26,10 +26,8 @@ let persons=[
   }
 ]
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
+  const Id = Math.floor(Math.random()*10000)
+  return Id
 }
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -56,30 +54,30 @@ app.get('/api/persons/:id', (request, response) => {
 })
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body);
-    if (!body.content) {
+    if (!body.name) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
     }
-  
+
     const person = {
-      content: body.content,
-      important: Boolean(body.important) || false,
+      name: body.name,
+      number: body.number,
       id: generateId(),
     }
-  
+
     persons = persons.concat(person)
-  
     response.json(person)
   })
   
   app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.filter(persons => persons.id !== id)
-    persons=person
+//    persons=person
+      
     response.status(204).end()
 })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
