9e18313c8c1f04b440fc5d4105232e08cefe4743
JoseJames <JoseJamesvillalobosroque@gmail.com>
2024-05-08 23:38:29 -0500

    add dotenv

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,7 +1,7 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-if (process.argv.length<3){
-    console.log('give password as argument')
+if (process.argv.length < 3) {
+  console.log('give password as argument')
   
     process.exit(1)
 }
